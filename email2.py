9ec447106847dc61cd42a773f29a4eab93cb54fc
Harold Roncancio <hroncanciom@systhinkers.co>
2017-02-26 22:39:40 -0500

    Se agrega la lectura de archivo de accounts y opts
    Se agrega la poribilidad de leer los accounts desde archivo y la
    capacidad de pasar opciones por linea de comando al script.

--- email2.py
+++ email2.py
@@ -2,6 +2,7 @@
 #https://gist.github.com/robulouski/7441883
 #
 import sys
+import getopt
 import imaplib
 import getpass
 import email
@@ -9,8 +10,6 @@ import email.header
 import datetime
 import re
 
-
-EMAIL_ACCOUNT = "youruser@gmail.com"
 
 # Use 'INBOX' to read inbox.  Note that whatever folder is specified, 
 # after successfully running this script all emails in that folder 
@@ -35,7 +34,7 @@ def process_mailbox(M):
             print("ERROR getting message", num)
             return
 
-        msg = email.message_from_bytes(data[0][1])
+        msg = email.message_from_string(data[0][1])
         hdr = email.header.make_header(email.header.decode_header(msg['FROM']))
         from_mail = str(hdr)
         print('Message %s: %s' % (num, from_mail))
@@ -43,27 +42,67 @@ def process_mailbox(M):
         print('BCC:', msg['BCC'])
 
 
-M = imaplib.IMAP4_SSL('imap.gmail.com')
+def login_mailbox(account):
+    M = imaplib.IMAP4_SSL(account[0])
 
-try:
-    rv, data = M.login(EMAIL_ACCOUNT, "password")
-except imaplib.IMAP4.error:
-    print ("LOGIN FAILED!!! ")
-    sys.exit(1)
+    try:
+        rv, data = M.login(account[2], account[3])
+    except imaplib.IMAP4.error:
+        print ("LOGIN FAILED!!! ")
+        sys.exit(1)
 
-print(rv, data)
+    print(rv, data)
 
-rv, mailboxes = M.list()
-if rv == 'OK':
-    print("Mailboxes:")
-    print(mailboxes)
+    rv, mailboxes = M.list()
+    if rv == 'OK':
+        print("Mailboxes:")
+        print(mailboxes)
 
-rv, data = M.select(EMAIL_FOLDER)
-if rv == 'OK':
-    print("Processing mailbox...\n")
-    process_mailbox(M)
-    M.close()
-else:
-    print("ERROR: Unable to open mailbox ", rv)
+    rv, data = M.select(EMAIL_FOLDER)
+    if rv == 'OK':
+        print("Processing mailbox...\n")
+        process_mailbox(M)
+        M.close()
+    else:
+        print("ERROR: Unable to open mailbox ", rv)
 
-M.logout()
+    M.logout()
+
+def main(argv):
+    inputfile = ''
+    outputfile = ''
+    excludefile = ''
+    aaccounts = list()
+
+    try:
+      opts, args = getopt.getopt(argv,"hi:o:e:",["ifile=","ofile=","efile="])
+    except getopt.GetoptError:
+      print sys.argv[0]," -i <inputfile> [-o <outputfile> [-e <excludefile>]]"
+      sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print sys.argv[0]," -i <inputfile> [-o <outputfile> [-e <excludefile>]]"
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+        elif opt in ("-e", "--efile"):
+            excludefile = arg
+    if inputfile == "":
+        print sys.argv[0]," -i <inputfile> [-o <outputfile> [-e <excludefile>]]"
+        sys.exit();
+    print "Input file is:", inputfile
+    print "Output file is:", outputfile
+    print "Exclude file is:", excludefile
+    with open(inputfile) as f:
+        accounts = f.readlines()
+    # you may also want to remove whitespace characters like `\n` at the end of each line
+    accounts = [x.strip() for x in accounts] 
+    for account in accounts:
+        aaccount = account.split("|")
+        login_mailbox(aaccount)
+        print aaccount[2]
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
